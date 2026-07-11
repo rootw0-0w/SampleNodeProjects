3b1f09c57be4e6115f3af3f507e72eb049603d6b
Alexa Sararana <alexa80804@gmail.com>
2026-07-11 13:42:40 +0800

    changes

--- update.sum.test.js
+++ update.sum.test.js
@@ -1,6 +1,6 @@
 const sum = require("./sum");
-test("adds 1 + 2 to equal 3", () => {
-  expect(sum(1, 2)).toBe(3);
+test("adds 6 + 4 to equal 10", () => {
+  expect(sum(6, 4)).toBe(10);
 });
 test("adds 5 + 7 to equal 12", () => {
   expect(sum(5, 7)).toBe(12);
